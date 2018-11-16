febd479a6001214456310ab95d558b0fd88fed87
Ryosuke-Kimoto <feedback.rk@gmail.com>
2018-11-16 17:14:27 +0900

    commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# docker-phpmyadmin
+# phpmyadmin
+
+phpmyadminをdockerで動かす  
+データベースに直接アクセスできる環境に限ります。  
+
+## 設定は編集してください
+config.user.inc.phpは自分で埋めてください。  
+
+## URL
+Win：http://192.168.99.100:9090  
+Mac：http://localhost:9090  
---
+++ config.user.inc.php
@@ -0,0 +1,11 @@
+<?php
+$i = 0;
+
+$i++;
+$cfg['Servers'][$i]['auth_type'] = 'config';
+$cfg['Servers'][$i]['verbose'] = 'viewname';
+$cfg['Servers'][$i]['host'] = 'database_hostname';
+$cfg['Servers'][$i]['user'] = 'database_user';
+$cfg['Servers'][$i]['password'] = 'database_password';
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
